670e4c92d48711a0169fc2d140e1296f1a31e877
Martin Loesch <martin@martinloesch.net>
2023-05-19 17:50:54 +0200

    Fixed and cleaned version command implementation.

--- cmd/version.go
+++ cmd/version.go
@@ -5,23 +5,36 @@ package cmd
 
 import (
 	"fmt"
+	"runtime/debug"
 
 	"github.com/dukemarty/adr-go/utils"
 	"github.com/spf13/cobra"
 )
 
+const VERSION = "0.1.0-beta.1"
+
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Output the version number",
-	Long:  `Output the version number.`,
+	Long: `Output information about the program version:
+	version number and, if available, git revision.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		verbose, _ := cmd.Flags().GetBool("verbose")
 
 		logger := utils.SetupLogger(verbose)
 		logger.Println("Command 'version' called.")
 
-		fmt.Printf("%s\n", "0.1.0-beta.1")
+		revisionInfo := ""
+		if bi, ok := debug.ReadBuildInfo(); ok {
+			for _, setting := range bi.Settings {
+				if setting.Key == "vcs.revision" {
+					revisionInfo = fmt.Sprintf(" (git revision: %s)", setting.Value)
+				}
+			}
+		}
+
+		fmt.Printf("%s%s\n", VERSION, revisionInfo)
 	},
 }
 
